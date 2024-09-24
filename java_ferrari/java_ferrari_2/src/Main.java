94c2fadd8fb751f8ec35e5ad156178ba84e36adc
Eduardo Ferrari <127300722+eduarif@users.noreply.github.com>
2024-09-24 09:13:25 -0300

    Add files via upload

---
+++ java_ferrari/java_ferrari_2/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Esse é o Screen Match");
+
+        System.out.printf("Filme: Como Treinar seu Dragão");
+
+        int anoDeLancamento = 2010;
+
+        System.out.printf("ano de lancamento: " + anoDeLancamento);
+    }
+}
